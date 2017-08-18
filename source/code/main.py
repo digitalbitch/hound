3f72fc886151d55c5de9cdd4b8adedd58b54a4cf
root <root@satin.goldbox>
2017-08-18 10:22:38 +0100

    initial code form blackhatpython hand typed

---
+++ source/code/main.py
@@ -0,0 +1,21 @@
+import socket
+import os
+
+# host to listen on 
+host = "192.168.0.196"
+
+# create a raw socket and bind it to the public interface 
+if os.name=="nt":
+    socket_protocol=socket.IPPROTO_IP
+else:
+    socket_protocol=socket.IPPROTO_ICMP 
+
+sniffer=socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+sniffer.bind((host, 0))
+
+# we want the ip headers included in the capture 
+snifer.setsockopt(socket.IPPROTO_IP, SOCKET.IP_HDRINCL, 1)
+
+# if were using Windows, we need to send an IOCTL
+# to set up permiscuous mode 
